8b230bc30e58f34ceea9d5823e4b8f2081363ae0
tolgaand <tolgaand@yandex.com>
2022-09-16 16:12:26 +0300

    Add create collection button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useState } from "react";
+import { CreateCollection } from "./CreateCollection";
+import { WalletConnectButton } from "./WalletConnectButton";
 
 export const Header = () => {
+  const [createCollectionOpen, setCreateCollectionOpen] = useState(false);
+
   return (
     <header className="bg-white shadow">
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -8,9 +12,22 @@ export const Header = () => {
           <span className="py-2 px-4 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-orange-500 relative inline-block">
             <span className="relative text-white text-lg">dailsap.</span>
           </span>
-          <WalletMultiButton />
+          <div className="flex gap-2">
+            <button
+              onClick={() => setCreateCollectionOpen(true)}
+              className="py-2 px-4 bg-orange-500 hover:bg-orange-600 text-white rounded-md"
+            >
+              Create Collection
+            </button>
+
+            <WalletConnectButton />
+          </div>
         </div>
       </div>
+      <CreateCollection
+        open={createCollectionOpen}
+        setOpen={setCreateCollectionOpen}
+      />
     </header>
   );
 };
