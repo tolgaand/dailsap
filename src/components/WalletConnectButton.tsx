59c987f25da5694dab1e838701a760fa7f47c886
tolgaand <tolgaand@yandex.com>
2022-09-19 22:35:24 +0300

    Change button color

--- src/components/WalletConnectButton.tsx
+++ src/components/WalletConnectButton.tsx
@@ -2,6 +2,6 @@ import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 export const WalletConnectButton = () => {
   return (
-    <WalletMultiButton className="inline-flex w-full justify-center rounded-md border border-transparent bg-orange-500 hover:bg-orange-600 px-4 py-2 text-base font-medium text-white shadow-sm focus:outline-none focus:ring-2  focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm h-auto" />
+    <WalletMultiButton className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-500 hover:bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm focus:outline-none focus:ring-2  focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm h-auto" />
   );
 };
