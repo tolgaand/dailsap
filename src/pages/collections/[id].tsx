bdbc2e804a753a8656d5a05a1ccd8d40d61b3d6d
tolgaand <tolgaand@yandex.com>
2022-09-20 17:35:37 +0300

    Add collection detail page

---
+++ src/pages/collections/[id].tsx
@@ -0,0 +1,8 @@
+import { Collections as _Collections } from "containers/Collections";
+import type { NextPage } from "next";
+
+const Collections: NextPage = () => {
+  return <_Collections />;
+};
+
+export default Collections;
